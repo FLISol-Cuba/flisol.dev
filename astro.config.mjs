7416e030708a5c33ecca427e99520e31c6aa2add
Manuel Ernesto Garcia <merodriguez9112@gmail.com>
2024-04-19 01:53:54 -0400

    updating astro site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://FLisol.dev"
+  site: 'https://flisol-cuba.github.io',
 });
