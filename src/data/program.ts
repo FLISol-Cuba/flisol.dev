80160c7135a45129456399c460f6640f1de263f5
Lázaro Armando <2146818+uranio-235@users.noreply.github.com>
2026-04-05 20:17:36 +0000

    Refactor: mejorar descripciones y títulos de presentaciones en program.ts

--- src/data/program.ts
+++ src/data/program.ts
@@ -3,18 +3,18 @@ export const program = [
     time: "9:00 am - 9:10 am",
     title: "MailAD: Server de correo para Cuba, hecho por cubanos.",
     speaker: "Pavel Milanes (CO7WT) ",
-    description: "Compatriota del territorio nacional. Representando a los radioaficionados. Nos va a decir como hacerte tu propio servidor email mambí."
+    description: "Representando a los radioaficionados. Nos va a decir como hacerte tu propio servidor email mambí."
   },
   {
     time: "9:10 am - 9:20 am",
-    title: "aws-doctor: De la terminal a FinOps – Auditoría de desperdicios en AWS y reportes de costos nativos.",
+    title: "aws-doctor: De la terminal a FinOp",
     speaker: "Juan Gabriel Companioni",
-    description: "Representando a La Taberna de la Jipansia."
+    description: "Representando a La Taberna de la Jipansia. Auditoría de desperdicios en AWS y reportes de costos nativos."
   },
   {
     time: "9:20 am - 9:30 am",
     title: "Zextras Carbonio CE como alternativa a Zimbra en Cuba.",
     speaker: "Alejandro Hernández Pastora",
-    description: "Representando a la comunidad NETWORLD."
+    description: "Representando a la comunidad NETWORLD. Nos va a decir como darle una patada a Zimbra y usar Zextras carbonio en su lugar."
   }
 ] as const;
